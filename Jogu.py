f34984a336c07506d69897261005f18872a1e23b
lf2knet <37741381+lf2knet@users.noreply.github.com>
2020-05-18 17:56:32 +0800

    Add files via upload

---
+++ Jogu.py
@@ -0,0 +1,54 @@
+﻿import random
+
+class Board:
+    def __init__(self):
+        self.field = [([],[],[],[],[],[]),([],[],[],[],[],[])]
+        
+    def refresh():
+        pass
+    def setting():
+        pass
+    def round_actions():
+        pass
+
+class Player:
+    def __init__(self,opt:class):
+        self.health = 30
+        self.morale = 30
+        self.win = 0
+        self.hand = list()
+        self.field_deck = [1,1,2,2,3,3,4,4,5,5,6,6]
+        self.current_field = 0
+        self.deck = list()
+        self.graveyard = list()
+        self.faction = opt
+        
+    def hand_draw(self):
+        n = 7 - len(self.hand)
+        self.hand.append(self.deck[0:n])
+        self.deck[0:n] = []
+        
+    def hand_discard(self):
+        ans = input('有想丟掉的卡嗎?(y/n)')
+        while ans == 'y':
+            i = int(input('想丟掉哪一張卡呢?(1~%d)'%(len(self.hand))))
+            self.hand.pop(i-1)
+            ans = input('還有想丟的卡嗎?(y/n)')
+            
+    def field_decide(self):
+        self.current_field = 1
+    
+    def deploy(self):
+        card =  input('選擇一張你想要打的牌')
+
+class Engineers():
+    def __init__(self,opt):
+        if opt == 'Society of Engineers':
+            pass
+            
+        elif opt == 'Guards of Keion':
+            self.element['Guardian Agel'] = [2,1]
+        
+        elif opt == 'Tribe Wu':
+            pass
+        
---
+++ main.py
@@ -0,0 +1,42 @@
+﻿import Jogu as Game
+
+#Game Setting
+board = Game.Board()
+factionA = input('A玩家，請問你要選擇哪一個陣營呢?\n(Society of Engineers,Guards of Keion, Tribe Wu)\n:')
+playerA = Game.Player(factionA)
+factionB = input('B玩家，請問你要選擇哪一個陣營呢?\n(Society of Engineers,Guards of Keion, Tribe Wu)\n:')
+playerB = Game.Player(factionB)
+
+Rd = 1
+Phase = 0
+
+#Game Start
+while Rd != 8:
+    #Phase 1 丟抽手牌
+    Phase = 1
+    if Rd == 1:
+        playerA.hand_draw()
+        playerB.hand_draw()
+    else:
+        print('A的回合:')
+        playerA.hand_discard()
+        playerA.hand_draw()
+        print('B的回合:')
+        playerB.hand_discard()
+        playerB.hand_draw()
+    
+    #Phase 2 抽戰場
+    Phase = 2
+    
+    
+    #Phase 3 佈署階段
+    Phase = 3
+    #Phase 4 結算階段
+    Phase = 4
+    #Phase 5 清理戰場
+    Phase = 5
+    
+    Rd += 1
+    print(Rd)
+    
+
